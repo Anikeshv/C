0c762c77d5ee017504b71e90049a910224ae9927
Anikeshv <anikeshverma26@gmail.com>
2025-03-31 22:19:11 +0530

    Did changes in the code

--- Learn.c
+++ Learn.c
@@ -4,15 +4,13 @@ int main(){
     return 0;
 }*/
 
-
+// -----------------------------------------------------------------------------------------------------------------------------------
 
 // Written using escape sequence (\n)
 // \n used to move cursor to next line
 // \t used to Creates a horizontal tab
 // \" used to  Inserts a double quote character
 // \\ used to Inserts a backslash character (\)
-
-
 
 /*#include <stdio.h>
 int main(){
@@ -37,14 +35,16 @@ int main(){
 return 0;
 }*/
 
+// -----------------------------------------------------------------------------------------------------------------------------------
 
 // Printed values in different datatypes
-/*#include <stdio.h>
 
+/*#include <stdio.h>
 int main(){
     int integer;
     float flo;
     char character;
+
     flo=3.5;
     integer=10;
     character='a';
@@ -57,10 +57,16 @@ return 0;
 
 }*/
 
+/*#include <stdio.h>
+int main() {
+  int myNum = 15;
+  char myLetter = 'D';
+  printf("My number is %d and my letter is %c", myNum, myLetter);
 
+  return 0;
+}*/
 
-
-
+// -----------------------------------------------------------------------------------------------------------------------------------
 
 
 
