cb774c72319d1e528f2a6df454f089aff5a61a6a
Anikeshv <anikeshverma26@gmail.com>
2025-03-31 20:07:28 +0530

    Changed some code

--- Learn.c
+++ Learn.c
@@ -12,28 +12,28 @@ int main(){
 // \" used to  Inserts a double quote character
 // \\ used to Inserts a backslash character (\)
 
-/*#include <stdio.h>
+#include <stdio.h>
 int main(){
     printf("Hey boy \n");
     printf("Lisht weight!");
     printf("Hello World!\nI am learning C.\nAnd it is awesome!");
 
-    printf("\n \n \n")
+    printf("\n \n \n");
 
     printf("Hello World!\t");
     printf("I am learning C.");
 
-    printf("\n \n \n")
+    printf("\n \n \n");
 
     printf("They call him \"Johnny\".");
 
-    printf("\n \n \n")
+    printf("\n \n \n");
 
     printf("Hello World!\\");
     printf("I am learning C.");
 
 return 0;
-}*/
+}
 
 
 
