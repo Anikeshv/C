a5cbf0ad5b9f636f7ce47157122f0a82566d1c48
Anikeshv <anikeshverma26@gmail.com>
2025-03-31 20:05:27 +0530

    Added some code

--- Learn.c
+++ Learn.c
@@ -4,10 +4,37 @@ int main(){
     return 0;
 }*/
 
-#include <stdio.h>
+
+
+// Written using escape sequence (\n)
+// \n used to move cursor to next line
+// \t used to Creates a horizontal tab
+// \" used to  Inserts a double quote character
+// \\ used to Inserts a backslash character (\)
+
+/*#include <stdio.h>
 int main(){
     printf("Hey boy \n");
     printf("Lisht weight!");
-return 0;
+    printf("Hello World!\nI am learning C.\nAnd it is awesome!");
 
-}
+    printf("\n \n \n")
+
+    printf("Hello World!\t");
+    printf("I am learning C.");
+
+    printf("\n \n \n")
+
+    printf("They call him \"Johnny\".");
+
+    printf("\n \n \n")
+
+    printf("Hello World!\\");
+    printf("I am learning C.");
+
+return 0;
+}*/
+
+
+
+
