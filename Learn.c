75c0354c81968edb088215ed161795614fce012b
Anikeshv <anikeshverma26@gmail.com>
2025-03-31 19:52:49 +0530

    Writeen code using escape sequence

--- Learn.c
+++ Learn.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main(){
     printf("Hey i am learning c");
     return 0;
+}*/
+
+#include <stdio.h>
+int main(){
+    printf("Hey boy \n");
+    printf("Lisht weight!");
+return 0;
+
 }
