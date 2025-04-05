56079b22e977a08c0300ae60da5c87be2774f054
Anikeshv <anikeshverma26@gmail.com>
2025-04-06 00:55:57 +0530

    While loop usecase

--- Learn.c
+++ Learn.c
@@ -451,7 +451,27 @@ return 0;
 
 // -----------------------------------------------------------------------------------------------------------------------------------
 
+// While usecase
+/*#include <stdio.h>
+int main() {
+  // A variable with some specific numbers
+  int numbers = 12345;
+  
+  // A variable to store the reversed number
+  int revNumbers = 0;
 
+  // Reverse and reorder the numbers
+  while (numbers) {
+    // Get the last number of 'numbers' and add it to 'revNumber'
+    revNumbers = revNumbers * 10 + numbers % 10;
+    // Remove the last number of 'numbers'
+    numbers /= 10;
+  }
+  // Output the reversed numbers
+  printf("%d", revNumbers);
+
+  return 0;
+}*/
 
 
 
