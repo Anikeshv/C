f2f09179043ac9875284d734200dc0f80c70cbff
Anikeshv <anikeshverma26@gmail.com>
2025-04-06 00:45:07 +0530

    While/Do while loops

--- Learn.c
+++ Learn.c
@@ -427,7 +427,28 @@ int main(){
 
 // -----------------------------------------------------------------------------------------------------------------------------------
 
+// While loops //
 
+/*#include <stdio.h>
+int main(){
+    int i=0;
+    while (i<20){
+        printf("I LOVE YOU\n");
+        ++i;
+    }
+}*/
+
+// Do while loop
+/*#include <stdio.h>
+int main() {
+  int i = 0;
+  do {
+    printf("%d\n", i);
+    i++;
+  }
+  while (i < 5);
+return 0;
+}*/
 
 
 
