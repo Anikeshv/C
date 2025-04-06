7a1bfb65e753af4f14c402c2cba896ce397b0f64
Anikeshv <anikeshverma26@gmail.com>
2025-04-06 10:28:01 +0530

    Started writing logic building codes in c

---
+++ Logic_codes.c
@@ -0,0 +1,16 @@
+// WAP to swap two numbers using a third variable //
+
+/*#include <stdio.h>
+int main(){
+    int a=5;
+    int b=7;
+    int c;
+
+    c=b;
+    b=a;
+    a=c;
+
+    printf("Value of a is %d\n",a);
+    printf("value of b is %d",b);
+return 0;
+}*/
