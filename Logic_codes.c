c07d0d4abbeea636e31c0ea9a1ef210695c8bfb6
Anikeshv <anikeshverma26@gmail.com>
2025-04-13 14:09:25 +0530

    Codes for pyramid

--- Logic_codes.c
+++ Logic_codes.c
@@ -14,3 +14,52 @@ int main(){
     printf("value of b is %d",b);
 return 0;
 }*/
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// WAP to print pyramid
+
+/*#include <stdio.h>
+int main(){
+    int i, j, rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    // Loop for rows
+    for(i=1; i<=rows; i++){
+        // Loop for spaces
+        for(j=1; j<=rows-i; j++){
+            printf(" ");
+        }
+        // Loop for asterisks
+        for(j=1; j<=2*i-1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}*/
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// WAP to print inverted pyramid
+
+/*#include <stdio.h>
+int main(){
+    int i,j, rows;
+    printf("Enter the number of rows:");
+    scanf("%d",&rows);
+
+    for (i=rows; i>0; i--){
+        // Loop for spaces
+        for (j=0; j<=(rows-i); j++){
+            printf(" ");
+        }
+        // Loop for asterisks
+        for (j=(2*i-1); j>0; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}*/
