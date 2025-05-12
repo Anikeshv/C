86703094362253b4548fdc23f14a84b216fb1541
Anikeshv <anikeshverma26@gmail.com>
2025-05-12 14:59:29 +0530

    Print elements using pointers

--- Learn.c
+++ Learn.c
@@ -867,7 +867,18 @@ int main() {
   return 0;
 }*/
 
+/*#include <stdio.h>
+ 
+int main() {
+  int myNumbers[4] = {25, 50, 75, 100};
+  int *ptr = myNumbers;
+  int i;
 
+  for (i = 0; i < 4; i++) {
+    printf("%d\n", *(ptr + i));
+  }
+  return 0;
+}*/
 
 
 
