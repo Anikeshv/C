57b2c9a1fca49e19c4561ed2b68c93a9f425f799
Anikeshv <anikeshverma26@gmail.com>
2025-05-12 14:58:10 +0530

    Pointers with array

--- Learn.c
+++ Learn.c
@@ -847,6 +847,26 @@ int main() {
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
+// Pointers in array
+
+/*#include <stdio.h>
+ 
+int main() {
+  int myNumbers[4] = {25, 50, 75, 100};
+  
+  // Get the value of the first element in myNumbers
+  printf("%d\n", *myNumbers);
+
+  // Get the value of the second element in myNumbers
+  printf("%d\n", *(myNumbers + 1));
+
+  // Get the value of the third element in myNumbers
+  printf("%d", *(myNumbers + 2));
+ 
+ 
+  return 0;
+}*/
+
 
 
 
