9734c790807e163ebcfb170bf5e95ee350770cc2
Anikeshv <anikeshverma26@gmail.com>
2025-05-13 12:19:54 +0530

    Returning functions

--- Learn.c
+++ Learn.c
@@ -947,6 +947,59 @@ int main() {
   return 0;
 }*/
 
+// Returning functions
+
+/*#include <stdio.h>
+
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  printf("Result is: %d", myFunction(5, 3));
+  return 0;
+}*/
+
+
+/*#include <stdio.h>
+
+// Function to convert Fahrenheit to Celsius
+float toCelsius(float fahrenheit) {
+  return (5.0 / 9.0) * (fahrenheit - 32.0);
+}
+
+int main() {
+  // Set a fahrenheit value
+  float f_value = 98.8;
+  
+  // Call the function with the fahrenheit value
+  float result = toCelsius(f_value);
+
+  // Print the fahrenheit value
+  printf("Fahrenheit: %.2f\n", f_value);
+  
+  // Print the result
+  printf("Convert Fahrenheit to Celsius: %.2f\n", result);
+
+  return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
