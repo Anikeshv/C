4f20114f948cf3884270ff77669e609851ee0d01
Anikeshv <anikeshverma26@gmail.com>
2025-05-14 22:55:50 +0530

    Recursion

--- Learn.c
+++ Learn.c
@@ -1019,7 +1019,26 @@ void myFunction() {
   printf("I just got executed!");
 }*/
 
+// ----------------------------------------------------------------------------------------------------------------------------
 
+// Recursions
+/*#include <stdio.h>
+
+int sum(int k);
+
+int main() {
+  int result = sum(10);
+  printf("%d", result);
+  return 0;
+}
+
+int sum(int k) {
+  if (k > 0) {
+    return k + sum(k - 1);
+  } else {
+    return 0;
+  }
+}*/
 
 
 
