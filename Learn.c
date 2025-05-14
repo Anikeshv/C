21c17f3a80cd2b18464d43d8ad9d5322142b1eee
Anikeshv <anikeshverma26@gmail.com>
2025-05-14 22:44:37 +0530

    Local and global variables

--- Learn.c
+++ Learn.c
@@ -984,7 +984,23 @@ int main() {
   return 0;
 }*/
 
+/*#include <stdio.h>
 
+// Global variable x
+int x = 5;
+
+void myFunction() {
+  // Local variable with the same name as the global variable
+  int x = 22;
+  printf("%d\n", x); // Refers to the local variable x
+}
+
+int main() {
+  myFunction();
+
+  printf("%d\n", x); // Refers to the global variable x
+  return 0;
+}*/
 
 
 
